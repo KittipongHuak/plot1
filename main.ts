df643d0192248fdc3e43de3b04d6cbe63dee1b41
KittipongHuak <97436073+KittipongHuak@users.noreply.github.com>
2022-04-07 09:53:07 +0700

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,5 @@
 namespace Lop {
-    /**
-     * สำหรับ นับค่า Encoder จากขา P8 
-     */
-    //% block 
+  //% block 
     export function Eop(Cnt: number) {
         let x = 0
         while (x < Cnt) {
