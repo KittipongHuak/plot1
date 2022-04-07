6c63ada402e9a1590f19b4549a751b3f12f58c23
KittipongHuak <97436073+KittipongHuak@users.noreply.github.com>
2022-04-07 09:48:59 +0700

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,8 @@
 namespace Sensor {
-
+    /**
+     * สำหรับ นับค่า Encoder จากขา P8 
+     */
+    //% block 
     export function Encoder(Cnt: number) {
         let x = 0
         while (x < Cnt) {
