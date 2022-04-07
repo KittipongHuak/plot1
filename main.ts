14be534ff43066dca622cdf08cec8da9f23cf069
KittipongHuak <97436073+KittipongHuak@users.noreply.github.com>
2022-04-07 09:25:26 +0700

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,4 @@
+//% weight=200 color=#ff0000 icon="\uf0e7"
 namespace gggg {
         export function fib(value: number): number {
         return value <= 1 ? value : fib(value - 1) + fib(value - 2);
