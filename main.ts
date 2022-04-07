9c4bf255979a0b9f49e907acef6e6a7219056421
KittipongHuak <97436073+KittipongHuak@users.noreply.github.com>
2022-04-07 09:37:38 +0700

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,3 @@
-/**
- * Use this file to define custom functions and blocks.
- * Read more at https://makecode.microbit.org/blocks/custom
- */
-
-/**
- * Custom blocks
- */
-//% weight=200 color=#ff0000 icon="\uf0e7"
 namespace Fenrn {
     /**
      * สำหรับ นับค่า Encoder จากขา P8 
