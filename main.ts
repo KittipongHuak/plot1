5abd32206935738a6d27b21a22ec54a3ad81bb94
KittipongHuak <97436073+KittipongHuak@users.noreply.github.com>
2022-04-07 09:45:14 +0700

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,24 @@
+/**
+ * Use this file to define custom functions and blocks.
+ * Read more at https://makecode.microbit.org/blocks/custom
+ */
 
+/**
+ * Custom blocks
+ */
+//% weight=200 color=#ff0000 icon="\uf0e7"
+namespace Sensor {
+    /**
+     * สำหรับ นับค่า Encoder จากขา P8 
+     */
+    //% block 
+    export function Encoder(Cnt: number) {
+        let x = 0
+        while (x < Cnt) {
+            while (pins.digitalReadPin(DigitalPin.P8) == 0) { }
+            x = x + 1
+            while (pins.digitalReadPin(DigitalPin.P8) == 1) { }
+            x = x + 1
+        }
+    }
+}
