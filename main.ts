1cad2c7c9be4f582623fa6cb20725cdabc8a7688
KittipongHuak <97436073+KittipongHuak@users.noreply.github.com>
2022-04-07 09:50:56 +0700

    Update main.ts

--- main.ts
+++ main.ts
@@ -6,10 +6,7 @@ namespace Sensor {
     export function Encoder(Cnt: number) {
         let x = 0
         while (x < Cnt) {
-            while (pins.digitalReadPin(DigitalPin.P8) == 0) { }
-            x = x + 1
-            while (pins.digitalReadPin(DigitalPin.P8) == 1) { }
-            x = x + 1
+
         }
     }
 }
